a9cd2e1e7931ff9e71f3ed1ce64d6cca4d3a5b34
timothyalton <mitchell.alton@live.com>
2020-03-02 13:55:44 -0600

    updated migrations

--- db/migrate/20200302191614_create_flights.rb
+++ db/migrate/20200302191614_create_flights.rb
@@ -1,7 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[6.0]
   def change
     create_table :flights do |t|
-
+      t.string :number
+      t.integer :terminal_id
       t.timestamps
     end
   end

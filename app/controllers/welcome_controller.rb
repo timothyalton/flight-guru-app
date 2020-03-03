84da9a148caeb72040ce933f0e31e61e274c4ced
placidowang <placidow@gmail.com>
2020-03-02 18:00:09 -0600

    things

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
     # url = 'http://api.aviationstack.com/v1/flights?access_key=16e6c3c05aa63a516a8b6ea32be8a235'
     url = 'http://api.aviationstack.com/v1/flights?access_key=16e6c3c05aa63a516a8b6ea32be8a235&flight_iata=WN1653'
 
-    pp @response = HTTParty.get(url)["data"]
+    pp @response = HTTParty.get(url)["data"][2]["arrival"]
 
   end
 end

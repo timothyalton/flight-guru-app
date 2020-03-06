d4407306b591d33a73d4745cf38f733bbe9d0f58
timothyalton <mitchell.alton@live.com>
2020-03-06 15:03:40 -0600

    fixing

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -6,9 +6,9 @@ class Terminal < ApplicationRecord
 
 # @restaurant_data_by_terminal = HTTParty.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Terminal+A+at+IAH&key=AIzaSyBE_4_oQR77YTzAD4d4Bh2LBWbGdB_mqzc')
 
-    def terminal_count
-        Terminal.all.count
-    end
+    # def terminal_count
+    #     Terminal.all.count
+    # end
 
-    
+
 end
